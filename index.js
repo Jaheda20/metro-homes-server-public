02e11318a2e4474fb90a76aa31cf046f9bdbff98
Jaheda20 <jahedasultana20@gmail.com>
2024-06-04 16:59:35 +0200

    made some apis for users

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ const port = process.env.PORT || 8000;
 app.use(cors())
 app.use(express.json())
 
+const verifyToken = (req, res, next) => {
+  // console.log(req.headers);
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: 'Unauthorized access' })
+  }
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'Unauthorized access' })
+    }
+    res.decoded = decoded;
+    next()
+  })
+
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.al6znur.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -28,26 +44,75 @@ const propertyCollection = client.db('metroHomes').collection('properties')
 
 
 // jwt related api
+app.post('/jwt', async (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_SECRET_KEY, { expiresIn: '365d' });
+  res.send({ token })
+})
+
 
 
 
 // user related APIs
 
-app.get('/users', async(req,res)=>{
+app.put('/user', async(req, res)=>{
+  const user = req.body;
+  const query = {email: user?.email}
+
+  // for existing users
+  const isExist = await userCollection.findOne(query)
+  if(isExist){
+    if(user.status === 'Requested'){
+      const result = await userCollection.updateOne(query, {
+        $set: {status: user?.status}
+      })
+      return res.send(result)
+    } else{
+      return res.send(isExist)
+    }
+  }
+    
+  // for new user
+  const options = {upsert: true}
+  const updateDoc = {
+    $set: {
+      ...user,
+      timeStamp: Date.now(),
+    }
+  }
+  const result = await userCollection.updateOne(query, updateDoc, options)
+  res.send(result)
+})
+
+app.get('/users', async (req, res) => {
   const result = await userCollection.find().toArray()
   res.send(result)
 })
 
-app.post('/users', async(req, res)=>{
-  const user = req.body;
-  const query = {email: user.email};
-  const existingUser = await userCollection.findOne(query)
-  if(existingUser){
-    return res.send({message: 'User Already Exists', insertedId: null})
-  }
-  const result = await userCollection.insertOne(user)
+app.get('/user/:email', async(req, res)=>{
+  const email = req.params.email;
+  const result = await userCollection.findOne({email})
   res.send(result)
 })
+
+app.patch('/users/update/:email', async (req, res) => {
+  const email = req.params.email
+  const user = req.body
+  const query = { email }
+  const updateDoc = {
+    $set: { ...user, timestamp: Date.now() },
+  }
+  const result = await userCollection.updateOne(query, updateDoc)
+  res.send(result)
+})
+
+app.delete('/deleteUser/:email', async (req, res) => {
+  const email = req.params.email;
+  const result = await userCollection.deleteOne({email});
+  res.send(result);
+})
+
+
 
 // property related apis
 app.get('/properties', async (req, res) => {
@@ -59,17 +124,17 @@ app.get('/properties', async (req, res) => {
   res.send(result)
 })
 
-app.get('/property/:id', async(req, res)=>{
+app.get('/property/:id', async (req, res) => {
   const id = req.params.id;
-  const query = {_id: new ObjectId(id)}
+  const query = { _id: new ObjectId(id) }
   const result = await propertyCollection.findOne(query)
   res.send(result)
 })
 
-app.get('/myAddedProperties/:email', async(req, res)=>{
+app.get('/myAddedProperties/:email', async (req, res) => {
   const email = req.params.email;
   console.log(email)
-  const query = {'agent.email':email}
+  const query = { 'agent.email': email }
   const result = await propertyCollection.find(query).toArray()
   res.send(result)
 })
@@ -81,9 +146,28 @@ app.post('/property', async (req, res) => {
   res.send(result)
 })
 
-app.delete('/property/:id', async(req, res)=>{
+app.put('/updateProperty/:id', async (req, res) => {
+  const id = req.params.id
+  const query = { _id: new ObjectId(id) }
+  const options = { upsert: true };
+  const updatedPropertyData = req.body;
+  console.log(updatedPropertyData)
+  const updateProperty = {
+    $set: {
+      image: updatedPropertyData.image,
+      location: updatedPropertyData.location,
+      title: updatedPropertyData.title,
+      min_price: updatedPropertyData.min_price,
+      max_price: updatedPropertyData.max_price
+    }
+  }
+  const result = await propertyCollection.updateOne(query, updateProperty, options)
+  res.send(result)
+})
+
+app.delete('/property/:id', async (req, res) => {
   const id = req.params.id;
-  const query = {_id: new ObjectId(id)}
+  const query = { _id: new ObjectId(id) }
   const result = await propertyCollection.deleteOne(query)
   res.send(result)
 })
