901cba08847924936ab49e72244d6eee10bcae3b
Jaheda20 <jahedasultana20@gmail.com>
2024-06-10 03:37:59 +0200

    implemented some api

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const jwt = require('jsonwebtoken')
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 const port = process.env.PORT || 8000;
@@ -12,22 +12,6 @@ const port = process.env.PORT || 8000;
 // middleware
 app.use(cors())
 app.use(express.json())
-
-const verifyToken = (req, res, next) => {
-  // console.log(req.headers);
-  if (!req.headers.authorization) {
-    return res.status(401).send({ message: 'Unauthorized access' })
-  }
-  const token = req.headers.authorization.split(' ')[1];
-  jwt.verify(token, process.env.ACCESS_SECRET_KEY, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({ message: 'Unauthorized access' })
-    }
-    res.decoded = decoded;
-    next()
-  })
-
-}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.al6znur.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -49,7 +33,6 @@ const offerCollection = client.db('metroHomes').collection('offers')
 const paymentCollection = client.db('metroHomes').collection('payments')
 
 
-
 // jwt related api
 app.post('/jwt', async (req, res) => {
   const user = req.body;
@@ -58,29 +41,75 @@ app.post('/jwt', async (req, res) => {
 })
 
 
+// verify token
+const verifyToken = (req, res, next) => {
+  console.log('inside verify token', req.headers.authorization);
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: 'Unauthorized access' })
+  }
+  const token = req.headers.authorization.split(' ')[1];
+  if (!token) {
+    return res.status(401).send({ message: 'No token given' })
+  }
+  jwt.verify(token, process.env.ACCESS_SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: 'Unauthorized access' })
+    }
+    req.decoded = decoded;
+    next()
+  })
+
+}
+
+// verify Admin
+const verifyAdmin = async (req, res, next) => {
+  const email = req.decoded.email;
+  console.log('admin', email)
+  const query = { email: email };
+  console.log('admin query', query)
+  const user = await userCollection.findOne(query)
+  const isAdmin = user?.role === 'Admin';
+  if (!isAdmin) {
+    return res.status(403).send({ message: 'Forbidden Access' })
+  }
+  next();
+}
+
+// verify agent
+const verifyAgent = async (req, res, next) => {
+  const email = req.decoded.email;
+  console.log('agent', email)
+  const query = { email: email };
+  const user = await userCollection.findOne(query);
+  const isAgent = user?.role === 'Agent';
+  if (!isAgent) {
+    return res.status(403).send({ message: 'Forbidden Access' });
+  }
+  next();
+}
+
 
 
 // user related APIs
 
-app.put('/user', async(req, res)=>{
+app.put('/user', async (req, res) => {
   const user = req.body;
-  const query = {email: user?.email}
+  const query = { email: user?.email }
 
   // for existing users
   const isExist = await userCollection.findOne(query)
-  if(isExist){
-    if(user.status === 'Requested'){
+  if (isExist) {
+    if (user.status === 'Requested') {
       const result = await userCollection.updateOne(query, {
-        $set: {status: user?.status}
+        $set: { status: user?.status }
       })
       return res.send(result)
-    } else{
+    } else {
       return res.send(isExist)
     }
   }
-    
   // for new user
-  const options = {upsert: true}
+  const options = { upsert: true }
   const updateDoc = {
     $set: {
       ...user,
@@ -91,21 +120,21 @@ app.put('/user', async(req, res)=>{
   res.send(result)
 })
 
-app.get('/users', async (req, res) => {
+app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
   const result = await userCollection.find().toArray()
   res.send(result)
 })
 
-app.get('/user/:email', async(req, res)=>{
+app.get('/user/:email', async (req, res) => {
   const email = req.params.email;
-  const result = await userCollection.findOne({email})
+  const result = await userCollection.findOne({ email })
   res.send(result)
 })
 
 app.patch('/users/update/:email', async (req, res) => {
   const email = req.params.email
   const user = req.body
-  const query = { email }
+  const query = { email: email }
   const updateDoc = {
     $set: { ...user, timestamp: Date.now() },
   }
@@ -113,16 +142,16 @@ app.patch('/users/update/:email', async (req, res) => {
   res.send(result)
 })
 
-app.delete('/deleteUser/:email', async (req, res) => {
+app.delete('/deleteUser/:email', verifyToken, verifyAdmin, async (req, res) => {
   const email = req.params.email;
-  const result = await userCollection.deleteOne({email});
+  const result = await userCollection.deleteOne({ email });
   res.send(result);
 })
 
 
 // property related apis
 
-app.get('/allProperties', async(req, res)=>{
+app.get('/allProperties', async (req, res) => {
   const result = await propertyCollection.find().toArray()
   res.send(result)
 })
@@ -135,15 +164,27 @@ app.get('/properties', async (req, res) => {
     location: { $regex: search, $options: 'i' },
     status: 'Verified'
   }
-  let options ={}
-  if (sort)
-    options = {sort: {min_price: sort === 'asc' ? 1 : -1}}
-  // if (sort){
-  //   options.sort = {min_price: sort === 'asc' ? 1 : -1}
-  // }
+  const sortOrder = sort === 'desc' ? -1 : 1;
+  const properties = await propertyCollection
+    .aggregate([
+      { $match: query },
+      {
+        $addFields: {
+          max_price: { $toDouble: "$max_price" },
+          min_price: { $toDouble: "$min_price" }
+        }
+      },
+      {
+        $addFields: {
+          priceDifference: { $subtract: ["$max_price", "$min_price"] }
+        }
+      },
+      { $sort: { priceDifference: sortOrder } }
+    ])
+    .toArray();
 
-  const result = await propertyCollection.find(query, options).toArray()
-  res.send(result)
+  res.send(properties);
+
 })
 
 
@@ -154,7 +195,7 @@ app.get('/property/:id', async (req, res) => {
   res.send(result)
 })
 
-app.get('/myAddedProperties/:email', async (req, res) => {
+app.get('/myAddedProperties/:email', verifyToken, verifyAgent, async (req, res) => {
   const email = req.params.email;
   console.log(email)
   const query = { 'agent.email': email }
@@ -174,11 +215,11 @@ app.put('/property/update/:id', async (req, res) => {
   const query = { _id: new ObjectId(id) }
   const propertyData = req.body;
   console.log(propertyData)
-  const options = {upsert: true}
+  const options = { upsert: true }
   const updateProperty = {
-    $set: 
-     { ...propertyData}
-    
+    $set:
+      { ...propertyData }
+
   }
   const result = await propertyCollection.updateOne(query, updateProperty, options)
   res.send(result)
@@ -191,12 +232,12 @@ app.delete('/property/:id', async (req, res) => {
   res.send(result)
 })
 
-app.patch('/property/status/:id', async(req, res)=>{
+app.patch('/property/status/:id', verifyToken, verifyAdmin, async (req, res) => {
   const id = req.params.id;
   const status = req.body.status;
-  const query = { _id: new ObjectId(id)}
+  const query = { _id: new ObjectId(id) }
   const updateDoc = {
-    $set: { status : status }
+    $set: { status: status }
   }
   const result = await propertyCollection.updateOne(query, updateDoc)
   res.send(result)
@@ -204,12 +245,12 @@ app.patch('/property/status/:id', async(req, res)=>{
 
 // review related apis
 
-app.get('/reviews', async(req, res)=>{
+app.get('/reviews', async (req, res) => {
   const result = await reviewCollection.find().toArray()
   res.send(result)
 })
 
-app.get('/myReviews/:email', async(req, res)=>{
+app.get('/myReviews/:email', async (req, res) => {
   const email = req.params.email;
   console.log(email)
   const query = { 'authorEmail': email }
@@ -217,21 +258,20 @@ app.get('/myReviews/:email', async(req, res)=>{
   res.send(result)
 })
 
-app.get('/reviews/:propertyId', async(req, res)=>{
+app.get('/reviews/:propertyId', async (req, res) => {
   const propertyId = req.params.propertyId;
-  
   const query = { 'propertyId': propertyId }
   const result = await reviewCollection.find(query).toArray()
   res.send(result)
 })
 
-app.post('/reviews', async(req, res)=>{
+app.post('/reviews', async (req, res) => {
   const reviewData = req.body;
   const result = await reviewCollection.insertOne(reviewData)
   res.send(result)
 })
 
-app.delete('/review/:id', async(req, res)=>{
+app.delete('/review/:id', async (req, res) => {
   const id = req.params.id;
   console.log(id)
   const query = { _id: new ObjectId(id) }
@@ -241,28 +281,28 @@ app.delete('/review/:id', async(req, res)=>{
 
 // wishlist related api
 
-app.get('/wishlists/:email', async(req, res)=>{
+app.get('/wishlists/:email', async (req, res) => {
   const email = req.params.email;
-  const query = {email: email}
+  const query = { email: email }
   const result = await wishlistCollection.find(query).toArray()
   res.send(result)
 })
 
-app.post('/wishlists/:email', async(req, res)=>{
+app.post('/wishlists/:email', async (req, res) => {
   const wishlistData = req.body;
-  const email= req.params.email;
+  const email = req.params.email;
   const propertyId = wishlistData.propertyId;
 
   console.log('wishlist data', wishlistData)
   console.log(email, propertyId)
 
-  const query ={
-    email: email, 
+  const query = {
+    email: email,
     propertyId: propertyId
   }
   console.log(query)
   const alreadyAdded = await wishlistCollection.findOne(query)
-  if(alreadyAdded){
+  if (alreadyAdded) {
     return res.status(400).send('You have already added')
   }
   const result = await wishlistCollection.insertOne(wishlistData)
@@ -272,27 +312,27 @@ app.post('/wishlists/:email', async(req, res)=>{
 
 // offer related api
 
-app.get('/offers', async(req, res)=>{
+app.get('/offers', async (req, res) => {
   const result = await offerCollection.find().toArray()
   res.send(result)
 })
 
-app.get('/offers/:email', async(req, res)=>{
+app.get('/offers/:email', async (req, res) => {
   const email = req.params.email;
-  const query = {email: email};
+  const query = { email: email };
   const result = await offerCollection.find(query).toArray()
   res.send(result)
 })
 
-app.get('/sentOffers/:agentEmail', async(req, res)=>{
+app.get('/sentOffers/:agentEmail', async (req, res) => {
   const email = req.params.agentEmail;
   console.log(email)
-  const query= {agentEmail: email}
+  const query = { agentEmail: email }
   const result = await offerCollection.find(query).toArray()
   res.send(result)
 })
 
-app.post('/offers', async(req, res)=>{
+app.post('/offers', async (req, res) => {
   const offerData = req.body;
   const query = {
     email: offerData.email,
@@ -300,12 +340,12 @@ app.post('/offers', async(req, res)=>{
   }
 
   const alreadyOffered = await offerCollection.findOne(query)
-  if(alreadyOffered){
-    return res.status(400).send({message: "You have already made an offer for this property"})
+  if (alreadyOffered) {
+    return res.status(400).send({ message: "You have already made an offer for this property" })
   }
 
-  if(offerData.amount < offerData.min_price || offerData.amount > offerData.max_price){
-    return res.status(400).send({message: "Offered amount must be within the price range"})
+  if (offerData.amount < offerData.min_price || offerData.amount > offerData.max_price) {
+    return res.status(400).send({ message: "Offered amount must be within the price range" })
   }
 
   const result = await offerCollection.insertOne(offerData)
@@ -313,12 +353,12 @@ app.post('/offers', async(req, res)=>{
 })
 
 
-app.patch('/offers/status/:id', async(req, res)=>{
+app.patch('/offers/status/:id', async (req, res) => {
   const id = req.params.id;
   const status = req.body.status;
-  const query = { _id: new ObjectId(id)}
+  const query = { _id: new ObjectId(id) }
   const updateDoc = {
-    $set: { status : status }
+    $set: { status: status }
   }
   const result = await offerCollection.updateOne(query, updateDoc)
   res.send(result)
@@ -327,14 +367,12 @@ app.patch('/offers/status/:id', async(req, res)=>{
 
 // payment intent
 
-
-
-app.post('/create-payment-intent', verifyToken, async(req, res)=>{
+app.post('/create-payment-intent', verifyToken, async (req, res) => {
   const { price } = req.body;
   const priceInCent = parseInt(price * 100);
   console.log(priceInCent, 'amount inside the intent')
-  
-  if(!price || priceInCent < 1) return
+
+  if (!price || priceInCent < 1) return
 
   const paymentIntent = await stripe.paymentIntents.create({
     amount: priceInCent,
@@ -347,28 +385,29 @@ app.post('/create-payment-intent', verifyToken, async(req, res)=>{
 })
 
 
-app.get('/payments', async(req, res)=>{
+app.get('/payments', async (req, res) => {
   const result = await paymentCollection.find().toArray()
   res.send(result)
 })
 
-app.get('/payment/:email', async(req, res)=>{
+app.get('/payment/:email', verifyToken, async (req, res) => {
   const email = req.params.email;
-  const query = {email: email}
+  const query = { email: email }
   const result = await paymentCollection.find(query).toArray()
   res.send(result)
 })
 
-app.get('/soldProperties/:agentEmail', async(req, res)=>{
+app.get('/soldProperties/:agentEmail', async (req, res) => {
   const email = req.params.agentEmail;
   console.log(email)
-  const query= {agentEmail: email}
+  const query = { agentEmail: email }
   const result = await paymentCollection.find(query).toArray()
   res.send(result)
+
 })
 
 // save payments
-app.post('/payments', async(req, res)=>{
+app.post('/payments', async (req, res) => {
   const paymentsData = req.body;
   const result = await paymentCollection.insertOne(paymentsData)
   res.send(result)
